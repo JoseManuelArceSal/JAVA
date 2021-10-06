52037fa53164904889767eb11e5bf871790aa752
JoseManuelArceSal <91504311+JoseManuelArceSal@users.noreply.github.com>
2021-10-05 20:38:41 -0500

    hola mundo
    primer trabajo en java  "hola mundo "

---
+++ holajava.java
@@ -0,0 +1,16 @@
+public class holajava {
+            /**
+            * Aplicacion java
+            *Title        :holajava
+            *Descripcion  :Hola mundo en java desde vim windows
+            *Author       :Tec Jose Manuel Arce
+            *Date         :20211005
+            *Version      :1
+            *Usage        : C:\>Javac /Path/holamundo_java.java
+            *             :C:\>Java hola_mundo_java
+            */
+
+       public static void main (String []args) {
+           System.out.println("Hola Mundo java JM"); // prints Holla mundo
+           }
+}
